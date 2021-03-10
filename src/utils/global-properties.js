c02700026befa21ce0ff9449c6f37a66969c7d82
shuwang.wu <shuwang.wu@getech.cn>
2021-03-10 13:57:49 +0800

    commit code

--- src/utils/global-properties.js
+++ src/utils/global-properties.js
@@ -3,12 +3,13 @@
  * @createdDate  : 2021-03-03 18:38:51
  * @version      : 1.0
  * @modifier     : shuwang.wu@getech.cn
- * @modifiedDate : 2021-03-03 18:55:47
+ * @modifiedDate : 2021-03-03 19:58:07
  * @reason       : set global properties
  * @FilePath     : \vite-demo\src\utils\global-properties.js
  */
 import momont from "moment";
 import { message } from "ant-design-vue";
+import { message } from "/@/utils/http";
 
 export function setGlobalProperties(app) {
   // configure message component
