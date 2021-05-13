894bd6756bb6f51e8d69da51c1cb7459a635ff59
shuwang_wu <shuwang_wu@kingdee.com>
2021-05-13 16:20:28 +0800

    commit(vue2.0): add ts in vue2.0

--- vue2.0/src/App.vue
+++ vue2.0/src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!-- <HelloWorld /> -->
+    <router-view />
   </div>
 </template>
 
-<script>
-export default {
-  name: 'App'
-}
+<script lang="ts">
+/* eslint-disable */
+
+import Vue from "vue";
+// import Component from "vue-class-component";
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+// @Component({ HelloWorld })
+export default class App extends Vue {}
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
